8b96b4ccdc3235b93e22724993759a491a6b4fa6
HSGill <harjotgill73@gmail.com>
2018-10-21 22:13:50 +1300

    update

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = () => {
-    return (<div className="db">
-        <button >
+const Button = ({onButClick}) => {
+    return (<div className='db'>
+        <button onClick={onButClick}>
         Add Task
         </button>
     </div>
---
+++ src/CardList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './Card';
+const CardList = ({tasks}) => {
+    return (  <div>
+               
+            } 
+    </div>
+    );
+}
+export default CardList;
