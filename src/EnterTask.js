562880063cae7268123dd630cc8db0fabe0f7edb
HSGill <harjotgill73@gmail.com>
2018-11-01 06:29:07 +1300

    fix issue

--- src/EnterTask.js
+++ src/EnterTask.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const EnterTask = ({searchfield, searchChange}) =>{
+const EnterTask = ({ searchChange}) =>{
     
 return (<div className="pa2 db">
 <input classname="pa3 ba b--green bg-lightest-blue"  type="" placeholder="Enter Task Here " 
 onChange = {searchChange}/>
+
 </div>)
 
 }
