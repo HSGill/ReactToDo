0a075a2b9abebfc66b59ecfb6999918856492c64
HSGill <harjotgill73@gmail.com>
2018-11-08 05:06:04 +1300

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import CardList from './CardList';
 import 'tachyons';
+
 class App extends Component {
   constructor() {
     super();
@@ -18,20 +19,20 @@ class App extends Component {
      const joined = this.state.tasks.concat(event.target.value);
      this.setState({tasks:joined});
   }
+  
   onButtonClick =() => {
 console.log ("working");
 
   }
 
   render() {
-    
     return (
-   
+
       <div className="tc ">
         <h1 className="f1 bg-green">To Do App</h1>
         <EnterTask searchChange={this.onSearchChange}  />
         
-        <CardList tasks ={this.state.tasks}/>
+        <CardList />
       </div>
     );
   }
