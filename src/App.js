5221161b0b462081f945d4a794366a47388c4b57
HSGill <harjotgill73@gmail.com>
2019-02-07 19:15:03 +1300

    sign In connection to database

--- src/App.js
+++ src/App.js
@@ -3,32 +3,81 @@ import './App.css';
 import NAVBAR from './NAVBAR.js';
 import CARD from './CARD.js';
 import Search from './Search';
-
+import Signin from './Signin.js';
+import SignUp from './SignUp.js';
 class App extends Component {
   constructor() {
     super();
     this.state = {
       searchfield: '',
-      addtask: []
+      addtask: [],
+      route:'signin',
+      isSignedIn:false,
+      user:{
+        id:'',
+        name:'',
+        email:'',
+        joined: new Date()
+      }
     }
   }
+
+loadUser = (data) =>{
+  this.setState({
+    user:{
+      id:data.id,
+      name:data.name,
+      email:data.email,
+      joined: new Date()
+    }
+  })
+}
+ 
   onInputChange = (event) => {
     event.preventDefault();
     this.setState({ searchfield: event.target.value });
+
     //console.log(event.target.value);
 
   }
   onSubmitTask = () => {
+    if(this.state.searchfield===''){
+      alert('Please add a task ');
+    }
+    else {
     this.state.addtask.push(this.state.searchfield);
+  }
     //this.setState.searchfield ='';
     // console.log(this.state.searchfield);
   }
-  render() {
+
+  onRouteChange = (route)=> {
+    if(route==='signout'){
+      this.setState({isSignedIn:false})
+    } else if(route==='home'){
+      this.setState({isSignedIn:true})
+    }
+    this.setState({route:route});
+  }
+
+    render() {
     return (
       <div className="App">
-        <NAVBAR />
+        <NAVBAR isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
+        {this.state.route==='home' 
+         ?
+        <div>
         <Search onInputChange={this.onInputChange} onSubmitTask={this.onSubmitTask} />
         <CARD addtask={this.state.addtask} />
+        </div>
+        :
+        (
+          this.state.route==='signin'
+        
+        ?<Signin onRouteChange = {this.onRouteChange} />
+         :<SignUp loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        )
+        }
       </div>
     );
   }
--- src/NAVBAR.js
+++ src/NAVBAR.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
-const NAVBAR = ()=> {
-    return (
-       <div> 
-       <h1> To Do App </h1>
-       
-       </div>
-    )
+const NAVBAR = ({ onRouteChange, isSignedIn }) => {
+
+if (isSignedIn) {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            
+                <p
+                    onClick={() => onRouteChange('signout')}
+                    className='f3 link dim black underlined pa3 pointer'> Sign Out</p>
+
+            </nav>);
+    }
+    
+    else {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <p
+                    onClick={() => onRouteChange('signin')}
+                    className='f3 link dim black underlined pa3 pointer'> Sign In</p>
+                <p
+                    onClick={() => onRouteChange('SignUp')}
+                    className='f3 link dim black underlined pa3 pointer'> Sign Up</p>
+
+
+            </nav>
+
+        );
+    }
+
 }
+
 
 export default NAVBAR;
